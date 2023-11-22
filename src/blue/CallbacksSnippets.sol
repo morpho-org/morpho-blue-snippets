1f1a5d249af2542207d00d7c03582df6d387131e
Tom Rpl <tomireppelin@gmail.com>
2023-11-22 10:38:50 +0100

    feat(swapper): removing useless comments

--- src/blue/CallbacksSnippets.sol
+++ src/blue/CallbacksSnippets.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity ^0.8.0;
 
-// import {SwapMock} from "@snippets/blue/mocks/SwapMock.sol";
 import {
     IMorphoSupplyCollateralCallback,
     IMorphoLiquidateCallback,
