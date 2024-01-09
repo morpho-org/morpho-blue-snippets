c4ee27d01b97bf749523ab6331cec081534b0f90
Tom Rpl <tomireppelin@gmail.com>
2024-01-09 17:49:03 +0100

    feat(ISWAP): harmonizing natspec for ISWAP

--- src/morpho-blue/interfaces/ISwap.sol
+++ src/morpho-blue/interfaces/ISwap.sol
@@ -1,22 +1,16 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-/**
- * @title ISwap
- * @dev Interface for the SwapMock contract.
- */
+/// @title ISwap
+/// @notice Interface for the SwapMock contract.
 interface ISwap {
-    /**
-     * @dev Swaps collateral token to loan token.
-     * @param amount The amount of collateral token to swap.
-     * @return returnedAmount The amount of loan token returned.
-     */
+    /// @notice Swaps collateral token to loan token.
+    /// @param amount The amount of collateral token to swap.
+    /// @return returnedAmount The amount of loan token returned.
     function swapCollatToLoan(uint256 amount) external returns (uint256 returnedAmount);
 
-    /**
-     * @dev Swaps loan token to collateral token.
-     * @param amount The amount of loan token to swap.
-     * @return returnedAmount The amount of collateral token returned.
-     */
+    /// @notice Swaps loan token to collateral token.
+    /// @param amount The amount of loan token to swap.
+    /// @return returnedAmount The amount of collateral token returned.
     function swapLoanToCollat(uint256 amount) external returns (uint256 returnedAmount);
 }
