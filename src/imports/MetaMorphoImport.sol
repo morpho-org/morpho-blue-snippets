bb6858016aa1467a94451af73d2423a7d5bbedbd
Reppelin Tom <tomireppelin@gmail.com>
2025-11-19 18:40:52 +0100

    feat(vault-v2): adding snippets

--- src/imports/MetaMorphoImport.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-// Copyright (c) 2025 Morpho Association
-pragma solidity 0.8.21;
-// Force foundry to compile MetaMorpho without importing it in the tests.
-
-import {MetaMorpho} from "../../lib/metamorpho/src/MetaMorpho.sol";
